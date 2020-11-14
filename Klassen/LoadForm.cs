4a40309fedc4591f7f3196b9d9cfff84f50671f8
Justin <74290179+PortaDouva@users.noreply.github.com>
2020-11-14 12:05:39 +0100

    Klassen Ordner hinzugefügt
    Hier werde ich alle Klassen hinzufügen welche ich erstelle zur Objektorientierung und Ihre Funktionsweise kurz erklären.
    
    Zu der ersten Klasse:
    Wie füge ich eine Klasse zu meinem Projekt hinzu?
    Fotoanleitung: https://prnt.sc/vj1kxf
    
    Wie benutze ich LoadForm?
    LoadForm stellt den Code aus der zuvor verwendeten Methode "OpenForm" bereit.
    In der Klasse sind zwei Methoden vorhanden. Hier wird unterschieden zwischen "OpenPanel" und "OpenTab"
    
    Möchte man also eine Form in ein Panel laden benötigt man folgenden Code:
    LoadForm.OpenPanel(new Form, myPanel);
    
    Selbiges gilt für eine TabPage, nur ersetze man hier das "myPanel" durch seine gewünschte TabPage.

---
+++ Klassen/LoadForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.OleDb;
+using System.Windows.Forms;
+
+namespace Spritzgussunternehmen
+{
+    class LoadForm
+    {
+        public static void OpenPanel(Form FormToOpen, Panel PanelToLoadIn)
+        {
+            FormToOpen.TopLevel = false;
+            FormToOpen.FormBorderStyle = FormBorderStyle.None;
+            FormToOpen.Dock = DockStyle.Fill;
+            PanelToLoadIn.Controls.Add(FormToOpen);
+            PanelToLoadIn.Tag = FormToOpen;
+            FormToOpen.BringToFront();
+            FormToOpen.Show();
+        }
+        public static void OpenTab(Form FormToOpen, TabPage TabToLoadIn)
+        {
+            FormToOpen.TopLevel = false;
+            FormToOpen.FormBorderStyle = FormBorderStyle.None;
+            FormToOpen.Dock = DockStyle.Fill;
+            TabToLoadIn.Controls.Add(FormToOpen);
+            TabToLoadIn.Tag = FormToOpen;
+            FormToOpen.BringToFront();
+            FormToOpen.Show();
+        }
+    }
+}
